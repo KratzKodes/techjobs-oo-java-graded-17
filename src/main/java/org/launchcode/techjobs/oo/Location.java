1c518c44445db205f23d870e5d02be09b1c15e13
Maurice Kratz <kratzkodes@gmail.com>
2023-09-30 16:15:35 -0400

    Completed part one of task one Location constructor

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -13,8 +13,11 @@ public class Location {
         nextId++;
     }
 
-    // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
-    //  constructor should also call the empty constructor in order to initialize the 'id' field.
+    //Second Constructor setting the value of Location
+    public Location(String value) {
+            this();
+            this.value = value;
+        }
 
 
     // Custom toString, equals, and hashCode methods:
